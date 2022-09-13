90eaaf6c955a82a2982793648c6f6c3d8f4f3c3f
Callum Macpherson <callummac@protonmail.com>
2022-09-13 19:54:06 +0100

    Setup base project

---
+++ src/components/addTaskForm/addTaskForm.test.js
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import AddTaskForm from "./addTaskForm";
+
+describe("AddTaskForm", () => {
+  it("can can call the addTask method when the add task button is clicked", () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+
+  });
+
+  it("can can call the addTask method when the form is submitted", () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+
+  });
+
+  it("can clear the input field when the form has been submitted", () => {
+    // Arrange
+    
+    // Act
+
+    // Assert
+
+  });
+});
+
