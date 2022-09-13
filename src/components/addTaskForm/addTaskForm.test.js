30a43660e6133dfeea31fb50c6c2900ea537a4b9
Callum Macpherson <callummac@protonmail.com>
2022-09-13 19:58:55 +0100

    Map out first test

--- src/components/addTaskForm/addTaskForm.test.js
+++ src/components/addTaskForm/addTaskForm.test.js
@@ -6,11 +6,20 @@ describe("AddTaskForm", () => {
   it("can call the addTask method when the add task button is clicked", () => {
     // Arrange
 
-
+        // 1. Setup mock variables
+        // 2. Render the component
 
     // Act
 
+        // 3. Find the input field
+        // 4. Enter text into the input field
+
+        // 5. Find the add task button
+        // 6. Click the button 
+
     // Assert
+
+        // 7. Assert that the parameter 'AddTask' was called with the description of the task
 
   });
 
