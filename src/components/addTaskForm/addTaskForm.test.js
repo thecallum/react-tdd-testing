7932b6a2e9c0ec38ce028ec83d8007895fe250ed
Callum Macpherson <callummac@protonmail.com>
2022-09-13 20:15:24 +0100

    Add initial test

--- src/components/addTaskForm/addTaskForm.test.js
+++ src/components/addTaskForm/addTaskForm.test.js
@@ -6,48 +6,54 @@ describe("AddTaskForm", () => {
   it("can call the addTask method when the add task button is clicked", () => {
     // Arrange
 
-        // 1. Setup mock variables
-        // 2. Render the component
+    // 1. Setup mock variables
+    const addTaskMock = jest.fn();
+
+    const inputText = "some task name";
+
+    // 2. Render the component
+    const { container } = render(<AddTaskForm addTask={addTaskMock} />);
 
     // Act
 
-        // 3. Find the input field
-        // 4. Enter text into the input field
+    // 3. Find the input field
+    const input = container.querySelector('[data-test="add-task-input"]');
 
-        // 5. Find the add task button
-        // 6. Click the button 
+    // 4. Enter text into the input field
+    userEvent.click(input);
+    userEvent.keyboard(inputText);
+
+    // 5. Find the add task button
+    const submitButton = container.querySelector(
+      '[data-test="add-task-button"]'
+    );
+
+    // 6. Click the button
+    userEvent.click(submitButton);
 
     // Assert
 
-        // 7. Assert that the parameter 'AddTask' was called with the description of the task
+    // 7. Assert that the parameter 'AddTask' was called with the description of the task
+    expect(addTaskMock).toHaveBeenCalledWith(inputText);
 
   });
 
   it("can call the addTask method when the form is submitted", () => {
     // Arrange
-
     // Act
-
     // Assert
-
   });
 
   it("can clear the input field when the form has been submitted", () => {
     // Arrange
-
     // Act
-
     // Assert
-
   });
 
   it("displays an alert message when the form is submitted with an empty input", () => {
     // Arrange
-
     // Act
-
     // Assert
-
-  })
+  });
 });
 
