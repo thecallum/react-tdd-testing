46760d3b6eef330c1b81873352fb4060afd6230f
Callum Macpherson <callummac@protonmail.com>
2022-09-16 09:48:27 +0100

    Pass first test

--- src/components/addTaskForm/addTaskForm.js
+++ src/components/addTaskForm/addTaskForm.js
@@ -1,14 +1,31 @@
-export default function AddTaskForm({}) {
+import { useState } from 'react'
+
+export default function AddTaskForm({ addTask }) {
+  const [taskDescription, setDescription] = useState("");
+
+  const handleAddTask = e => {
+    e.preventDefault();
+
+    addTask(taskDescription)
+  }
+
   return (
-    <form className="addTask-form">
+    <form className="addTask-form" onSubmit={handleAddTask}>
       <input
         type="text"
         name="addTaskInput"
         aria-label="Task description"
         className="addTask-input"
         placeholder="Add a task"
+        data-test="add-task-input"
+        onChange={e => setDescription(e.target.value)}
+        value={taskDescription}
       />
-      <button type="submit" className="addTask-button">
+      <button
+        type="submit"
+        className="addTask-button"
+        data-test="add-task-button"
+      >
         Add
       </button>
     </form>
