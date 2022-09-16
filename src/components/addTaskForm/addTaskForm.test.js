28787964dc4c5fef2bb772589d0e43ce5b85e0a6
Callum Macpherson <callummac@protonmail.com>
2022-09-16 11:49:42 +0100

    Add next test

--- src/components/addTaskForm/addTaskForm.test.js
+++ src/components/addTaskForm/addTaskForm.test.js
@@ -40,8 +40,39 @@ describe("AddTaskForm", () => {
 
   it("can clear the input field when the form has been submitted", () => {
     // Arrange
+
+    // 1. Setup mock variables
+    const addTaskMock = jest.fn();
+
+    const inputText = "some task name";
+
+    // 2. Render the component
+    const { container } = render(<AddTaskForm addTask={addTaskMock} />);
+
     // Act
+
+    // 3. Find the input field
+    const input = container.querySelector('[data-test="add-task-input"]');
+
+    // 4. Enter text into the input field
+    userEvent.click(input);
+    userEvent.keyboard(inputText);
+
+    // 5. Find the add task button
+    const submitButton = container.querySelector(
+      '[data-test="add-task-button"]'
+    );
+
+    // 6. Click the button
+    userEvent.click(submitButton);
+
     // Assert
+
+    // 7. Assert that the parameter 'AddTask' was called
+    expect(addTaskMock).toHaveBeenCalled();
+
+    // 8. Assert that the input field is empty
+    expect(input.value).toEqual("");
   });
 
   it("displays an alert message when the form is submitted with an empty input", () => {
